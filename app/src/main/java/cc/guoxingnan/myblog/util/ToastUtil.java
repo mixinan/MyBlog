e12d522d8e82a33360344f162c58af162bad3589
Tiipy <mixinan@163.com>
2016-06-02 08:10:01 +0800

    修复bug
    点击博文，首页闪退的问题：toastUtil.cancel()没有判断是否为null

--- app/src/main/java/cc/guoxingnan/myblog/util/ToastUtil.java
+++ app/src/main/java/cc/guoxingnan/myblog/util/ToastUtil.java
@@ -9,19 +9,21 @@ import android.widget.Toast;
 public class ToastUtil {
     private static Toast toast;
 
-    private ToastUtil(){
+    private ToastUtil() {
     }
 
-    public static void showToast(Context context, String string){
-        if (toast == null){
-            toast = Toast.makeText(context, string,Toast.LENGTH_SHORT);
-        }else{
+    public static void showToast(Context context, String string) {
+        if (toast == null) {
+            toast = Toast.makeText(context, string, Toast.LENGTH_SHORT);
+        } else {
             toast.setText(string);
         }
         toast.show();
     }
 
-    public static void cancelToast(){
-        toast.cancel();
+    public static void cancelToast() {
+        if (toast != null) {
+            toast.cancel();
+        }
     }
 }
